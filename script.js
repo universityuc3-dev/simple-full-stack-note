d5621cce4dee0b504fd0b484e00efa4f110164f2
universityuc3-dev <universityuc3@gmail.com>
2026-03-10 02:03:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const API = "/api/notes";
+
+async function loadNotes() {
+  const res = await fetch(API);
+  const notes = await res.json();
+
+  const list = document.getElementById("notesList");
+  list.innerHTML = "";
+
+  notes.forEach(note => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${note.text}
+      <button onclick="deleteNote(${note.id})">Delete</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+async function addNote() {
+  const input = document.getElementById("noteInput");
+
+  await fetch(API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      text: input.value
+    })
+  });
+
+  input.value = "";
+  loadNotes();
+}
+
+async function deleteNote(id) {
+  await fetch(API + "/" + id, {
+    method: "DELETE"
+  });
+
+  loadNotes();
+}
+
+loadNotes();
